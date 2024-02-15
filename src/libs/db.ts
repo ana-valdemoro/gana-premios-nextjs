67d668dea711a41ba2d2d6541c3ac4d51dffc540
Ana Valdemoro Ramirez <Ana@ADEdelivered.com>
2024-02-15 23:12:29 +0000

    feat: define database connection

---
+++ src/libs/db.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "../models/User";
+
+// define database connection
+export const AppDatSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [User],
+  synchronize: true,
+  logging: false,
+});
+
+export const connectDb = async () => {
+  try {
+    await AppDatSource.initialize();
+    console.log("Database connected");
+    return Promise.resolve(true);
+  } catch (error) {
+    console.error(error);
+    return Promise.resolve(false);
+  }
+};
