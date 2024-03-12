a068607949c09b4d4c5eff6a7970a39a1eb673ad
Ana Valdemoro Ramirez <ana_v_r97@hotmail.com>
2024-03-12 19:51:16 +0000

    WIP: implamenting jwt veryfy method

--- src/libs/jwt.ts
+++ src/libs/jwt.ts
@@ -1,5 +1,9 @@
 import jwt from 'jsonwebtoken';
-import { JWT_PRIVATE_KEY, JWT_ACSESS_TOKEN } from '../constants/accessToken';
+import {
+  JWT_PRIVATE_KEY,
+  JWT_ACSESS_TOKEN,
+  JWT_PUBLIC_KEY,
+} from '../constants/accessToken';
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 type Payload = {
@@ -17,4 +21,32 @@ export const generateJWT = (
   };
 
   return jwt.sign(payload, JWT_PRIVATE_KEY, jwtOptions);
+};
+
+// AGILIA PROJEC
+// export const authorize = (req, res, next) => {
+//   passport.authenticate('jwt', { session: false }, (user, error, info) => {
+//     if (error) {
+//       return res.status(401).send(error);
+//     }
+//     if (!user) {
+//       return res.status(401).send(info);
+//     }
+//     req.user = user;
+//     req.userType = info;
+//     return next();
+//   })(req, res);
+// };
+
+export const verify = (token: string): string | jwt.JwtPayload | Error => {
+  try {
+    // Decode the token using a secret key-phrase
+    return jwt.verify(token, JWT_PUBLIC_KEY, {
+      algorithms: [JWT_ACSESS_TOKEN.ALGORITHM as jwt.Algorithm],
+    });
+  } catch (error) {
+    // TODO: create custom error
+    // throw new AccessTokenInvalidError({ errorDescription: error.message });
+    throw new Error('JWT verification fail');
+  }
 };
