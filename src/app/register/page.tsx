5c37a7cd209a55e37c9d35045c25e34be3ccf3b5
Ana Valdemoro Ramirez <ana_v_r97@hotmail.com>
2024-04-02 13:10:36 +0100

    feat: style register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,42 @@
-import React, { ReactElement } from "react";
+import styles from './page.module.css';
 
-export const RegisterPage: React.FC = (): ReactElement => {
-  return <div>hellou register</div>;
-};
+function RegisterPage(): JSX.Element {
+  return (
+    <main className='o-page-container'>
+      {/* <form onSubmit={handleSubmit} > */}
+      <form className={styles.registerForm} >
+        <h1>Register your account</h1>
+
+        <label className={styles.registerForm__label}>Fullname</label>
+        <input
+          className={styles.registerForm__input}
+          type="text"
+          placeholder="Fullname"
+          name="fullname"
+        />
+
+        <label className={styles.registerForm__label}>Email</label>
+        <input
+          className={styles.registerForm__input}
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+
+        <label className={styles.registerForm__label}>Password</label>
+        <input
+          className={styles.registerForm__input}
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+
+        <button className={styles.registerForm__button}>
+          Signup
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default RegisterPage; 
